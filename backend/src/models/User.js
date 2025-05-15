f83d1eb36b681ef0e3bf4562f2b22c6451b79c4d
jabezinu <jabezzinu31@gmail.com>
2025-05-16 02:37:18 +0300

    make the signup controller

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+import  mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+
+const userSchema = new mongoose.Schema({
+    fullName: {type: String, required: true},
+    email: {type: String, required: true, unique: ture},
+    password: {type: String, required: true, minlength: 6},
+    bio: {type: String, default: ""},
+    profilePic: {type: String, default:""},
+    nativeLanguage: {type: String, default:""},
+    learningLanguage: {type: String, default:""},
+    location: {type: String, default:""},
+    isOnboarded:{type: Boolean, default: false},
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        }
+    ],
+}, 
+{timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+
+// Pre hook
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+export default User;
