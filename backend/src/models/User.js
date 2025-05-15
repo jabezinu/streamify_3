a2f638983091d840136c607be6fc345b29121e49
jabezinu <jabezzinu31@gmail.com>
2025-05-16 02:49:26 +0300

    fix password hash

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     fullName: {type: String, required: true},
-    email: {type: String, required: true, unique: ture},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true, minlength: 6},
     bio: {type: String, default: ""},
     profilePic: {type: String, default:""},
@@ -21,20 +21,20 @@ const userSchema = new mongoose.Schema({
 {timestamps: true}
 );
 
-const User = mongoose.model("User", userSchema);
 
 // Pre hook
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-
+    
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-
+        
         next();
     } catch (error) {
         next(error);
     }
 })
 
+const User = mongoose.model("User", userSchema);
 export default User;
