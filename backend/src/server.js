a226747b9f60c73085d1642d343ff23445744746
jabezinu <jabezzinu31@gmail.com>
2025-05-16 01:14:05 +0300

    call the DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 
 
@@ -11,5 +12,5 @@ app.use("/api/auth/", authRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port ", PORT);
-    
+    connectDB()
 })
