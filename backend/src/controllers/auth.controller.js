4860d0fb5fde70826536085f45201df7a0dc3ea6
jabezinu <jabezzinu31@gmail.com>
2025-05-16 04:09:39 +0300

    add password compare

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,8 +55,42 @@ export async function signup(req, res)  {
         res.status(500).json({message: "Internal Server Error"});        
     }
 }
-export function login(req, res) {
-    res.send("login Route.")
+export async function login(req, res) {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"})
+        }
+        
+        const isPasswordCorrect = await user.matchPassword(password)
+        if(isPasswordCorrect) {
+            return res.status(401).json({message: "Invalid email or password"})
+        }
+
+        const token = jwt.sign(
+            {userId: newUser._id}, 
+            process.env.JWT_SECRET_KEY, 
+            {expiresIn: "7d"}
+        )
+
+        res.cookie("token", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true, //prevent XSS attacks,
+            sameSite: "strict", // prevent CSRF attacks
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(200).json({success: true, user})
+    }catch(err){
+        console.log("Error in login controller", err.message);
+        res.status(500).json({message: "Internal Server Error"});        
+    }
+    
 }
 export function logout(req, res) {
     res.send("logout Route.")
