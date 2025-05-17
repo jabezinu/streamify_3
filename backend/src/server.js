11d72ae19f3f1e79dfcfec63ca76f5aec1ef486f
jabezinu <jabezzinu31@gmail.com>
2025-05-18 00:20:51 +0300

    add cookies parser

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,16 @@ import express from "express"
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 
 
 const app = express();
 const PORT = process.env.PORT
 
+// Parsers
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth/", authRoutes)
 
