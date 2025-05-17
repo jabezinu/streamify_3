5f92b898d4e6f4f4349a193aff1192ba6c4e40af
jabezinu <jabezzinu31@gmail.com>
2025-05-18 00:21:42 +0300

    add protecoter middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 
 const authRoutes = express.Router();
@@ -7,5 +8,7 @@ const authRoutes = express.Router();
 authRoutes.post("/signup", signup)
 authRoutes.post("/login", login)
 authRoutes.post("/logout", logout)
+
+authRoutes.post("/onboarding", protectRoute, onboard)
 
 export default authRoutes
