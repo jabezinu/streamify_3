3207cd45437ca286ff857789688ddd5164d44271
jabezinu <jabezzinu31@gmail.com>
2025-05-20 01:46:24 +0300

    done setting up daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
+    <h1 className="text-3xl font-bold underline text-red-500" data-theme="coffee">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </h1>
   )
 }
